3a8308c03a6863d46963afc38a3ce5158a9ea324
Juan Alegre <jpalegree@gmail.com>
2023-05-13 22:52:33 -0300

    Setup fragments with view binding

--- app/src/main/java/com/example/vendorlust/VendorFragment.kt
+++ app/src/main/java/com/example/vendorlust/VendorFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.vendorlust.databinding.FragmentVendorBinding
 
 
 class VendorFragment : Fragment(R.layout.fragment_vendor) {
 
+    private lateinit var binding: FragmentVendorBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentVendorBinding.bind(view)
     }
 
 }
