eb82c1a7d022db0b667b4211c396b3c013fb0332
Juan Alegre <jpalegree@gmail.com>
2023-05-14 20:38:26 -0300

    Create interface repo and repoImplement, object class with BASE_URL, webService and RetrofitClient

---
+++ app/src/main/java/com/example/vendorlust/repository/VendorRepoImplement.kt
@@ -0,0 +1,11 @@
+package com.example.vendorlust.repository
+
+import com.example.vendorlust.data.model.VendorList
+
+class VendorRepoImplement(): VendorRepository  {
+
+    override suspend fun getAllVendors(): VendorList {
+        TODO("Not yet implemented")
+    }
+
+}
