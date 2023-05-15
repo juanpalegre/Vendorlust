c8ce4487298e00476876683f3cdb7b621166d1d4
Juan Alegre <jpalegree@gmail.com>
2023-05-15 00:31:17 -0300

    refactor base_url

--- app/src/main/java/com/example/vendorlust/repository/WebService.kt
+++ app/src/main/java/com/example/vendorlust/repository/WebService.kt
@@ -3,14 +3,15 @@ package com.example.vendorlust.repository
 import com.example.vendorlust.application.AppConstants
 import com.example.vendorlust.data.model.VendorList
 import com.google.gson.GsonBuilder
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface WebService {
 
-    @GET
-    suspend fun getVendorList(): VendorList
+    @GET("vendorlust.json")
+    suspend fun getVendorList(): Response<VendorList>
 
 }
 
