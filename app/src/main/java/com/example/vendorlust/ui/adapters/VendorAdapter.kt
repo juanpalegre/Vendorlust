fb048074a6adc7c19202ba2a21da305f015fbad2
Juan Alegre <jpalegree@gmail.com>
2023-05-17 01:17:59 -0300

    working on OpeningHours logic

--- app/src/main/java/com/example/vendorlust/ui/adapters/VendorAdapter.kt
+++ app/src/main/java/com/example/vendorlust/ui/adapters/VendorAdapter.kt
@@ -2,13 +2,16 @@ package com.example.vendorlust.ui.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.vendorlust.core.BaseViewHolder
+import com.example.vendorlust.data.model.OpeningHours
 import com.example.vendorlust.data.model.Vendor
 import com.example.vendorlust.databinding.ItemVendorBinding
+import java.util.*
 
 class VendorAdapter(
     private var vendorList: List<Vendor>,
@@ -46,10 +49,53 @@ class VendorAdapter(
         notifyDataSetChanged()
     }
 
+    fun isVendorOpen(openingHours: OpeningHours): Boolean {
+        val currentDayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+        val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        val currentMinute = Calendar.getInstance().get(Calendar.MINUTE)
+
+        val todayOpeningTimes = when (currentDayOfWeek) {
+            Calendar.MONDAY -> openingHours.monday
+            Calendar.TUESDAY -> openingHours.tuesday
+            Calendar.WEDNESDAY -> openingHours.wednesday
+            Calendar.THURSDAY -> openingHours.thursday
+            Calendar.FRIDAY -> openingHours.friday
+            Calendar.SATURDAY -> openingHours.saturday
+            Calendar.SUNDAY -> openingHours.sunday
+            else -> emptyList()
+        }
+
+        return todayOpeningTimes.any { openingTime ->
+            val opensAt = openingTime.opensAt.split(":")
+            val closesAt = openingTime.closesAt.split(":")
+            val opensHour = opensAt[0].toInt()
+            val opensMinute = opensAt[1].toInt()
+            val closesHour = closesAt[0].toInt()
+            val closesMinute = closesAt[1].toInt()
+
+            val opensTime = Calendar.getInstance()
+            opensTime.set(Calendar.HOUR_OF_DAY, opensHour)
+            opensTime.set(Calendar.MINUTE, opensMinute)
+
+            val closesTime = Calendar.getInstance()
+            closesTime.set(Calendar.HOUR_OF_DAY, closesHour)
+            closesTime.set(Calendar.MINUTE, closesMinute)
+
+            opensTime.before(closesTime) &&
+                    currentHour >= opensHour && currentMinute >= opensMinute &&
+                    currentHour <= closesHour && currentMinute <= closesMinute
+        }
+    }
+
+
     private inner class VendorViewHolder(val binding: ItemVendorBinding, val context: Context): BaseViewHolder<Vendor>(binding.root){
         override fun bind(item: Vendor) {
             Glide.with(context).load(item.heroImage.url).centerCrop().into(binding.imgAvatar)
             binding.txtName.text = item.displayName
+            val isOpen = isVendorOpen(item.openingHours)
+            if (isOpen){
+                binding.txtIsOpen.visibility = View.VISIBLE
+            }
         }
 
     }
