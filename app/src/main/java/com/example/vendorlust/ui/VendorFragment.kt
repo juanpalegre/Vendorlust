f64bd230d099f8e41240c7608611a5ea8b6320f1
Juan Alegre <jpalegree@gmail.com>
2023-05-16 23:14:18 -0300

    Set VendorFragment.kt

--- app/src/main/java/com/example/vendorlust/ui/VendorFragment.kt
+++ app/src/main/java/com/example/vendorlust/ui/VendorFragment.kt
@@ -8,15 +8,17 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.vendorlust.R
 import com.example.vendorlust.core.Resource
+import com.example.vendorlust.data.model.Vendor
 import com.example.vendorlust.data.remote.VendorDataSource
 import com.example.vendorlust.databinding.FragmentVendorBinding
 import com.example.vendorlust.presentation.VendorViewModel
 import com.example.vendorlust.presentation.VendorViewModelFactory
 import com.example.vendorlust.repository.RetrofitClient
 import com.example.vendorlust.repository.VendorRepoImplement
+import com.example.vendorlust.ui.adapters.VendorAdapter
 
 
-class VendorFragment : Fragment(R.layout.fragment_vendor) {
+class VendorFragment : Fragment(R.layout.fragment_vendor), VendorAdapter.OnVendorClickListener {
 
     private lateinit var binding: FragmentVendorBinding
     private val viewModel by viewModels<VendorViewModel> { VendorViewModelFactory(VendorRepoImplement(
@@ -30,17 +32,27 @@ class VendorFragment : Fragment(R.layout.fragment_vendor) {
         viewModel.fetchVendors().observe(viewLifecycleOwner, Observer { result->
             when(result){
                 is Resource.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
                     Log.d("liveData", "Loading...")
                 }
                 is Resource.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.recyclerMain.adapter = VendorAdapter(result.data, this@VendorFragment)
                     Log.d("liveData", "${result.data}")
                 }
                 is Resource.Failure -> {
+                    binding.progressBar.visibility = View.GONE
                     Log.d("liveData", "${result.exception}")
                 }
             }
         })
 
+
+
+    }
+
+    override fun onVendorClick(vendor: Vendor) {
+        Log.d("Vendor", "OnVendorClick: $vendor")
     }
 
 }
